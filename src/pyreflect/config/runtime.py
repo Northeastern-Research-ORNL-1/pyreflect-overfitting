962dfd899941f1e1c72425e3ca81ad0d28acfed3
KrishnaRaheja <krishnarahejaprof@gmail.com>
2026-02-11 14:44:48 -0800

    FORCED PROGRAM TO USE CPU - the gpu on my laptop (5070ti) is too new for this project to use cuda

--- src/pyreflect/config/runtime.py
+++ src/pyreflect/config/runtime.py
@@ -1,4 +1,5 @@
 import torch
 
-DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+DEVICE = torch.device("cpu")  # Force CPU
+#DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 print(f'Selected device for model training: {DEVICE}')
